cd3765366479ca47fb79124aeabe9330ba952f47
MilosNesh <neskovic.milos02@gmail.com>
2024-11-05 23:04:28 +0100

    feat: implemented login

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LoginComponent,
+    FormsModule,
+    HttpClientModule
+  ],
+  exports:[
+    LoginComponent,
+    HttpClientModule
+  ]
+})
+export class UserModule { }
